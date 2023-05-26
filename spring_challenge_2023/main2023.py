459296e6fb232d1beab0a3563b1bb6645b05117c
fortes23 <brais.fortes@gmail.com>
2023-05-26 18:49:40 +0200

    feat: Refactor Spring Challenge 2023 initial code

--- spring_challenge_2023/main2023.py
+++ spring_challenge_2023/main2023.py
@@ -8,6 +8,7 @@ def debug(msg):
     if DEBUG:
         print(msg, file=sys.stderr)
 
+
 class CellType(Enum):
     OTHER = 0
     EGGS = 1
@@ -16,66 +17,99 @@ class CellType(Enum):
     def __int__(self):
         return self.value
 
+
 class CellInfo:
 
-    def __init__(self, idx, type, initial_resources):
+    def __init__(self, idx, type, initial_resources, neighs):
         self.idx = idx  # Cell index
         self.type = CellType(type)  # Cell type
         self.resources = initial_resources  # Initial resources
+        self.my_ants = 0  # Amount of your ants on this cell
+        self.opp_ants = 0  # Amount of opponent ants on this cell
+        self.neighs = neighs  # Neighbouring cells
 
-    def update(self, resources):
+    def __lt__(self, other):
+        return self.resources < other.resources
+
+    def update(self, resources, my_ants, opp_ants):
         self.resources = resources
-        # debug(f"Cell[{self.idx}] type[{self.type.value}] resources[{self.resources}]")
+        self.my_ants = my_ants
+        self.opp_ants = opp_ants
 
 
-number_of_cells = int(input())  # amount of hexagonal cells in this map
+class Map:
+    def __init__(self):
+        self.number_of_cells = int(input())  # amount of hexagonal cells in this map
+        self.cells = []
+        for i in range(self.number_of_cells):
+            # _type: 0 for empty, 1 for eggs, 2 for crystal
+            # initial_resources: the initial amount of eggs/crystals on this cell
+            # neigh_0: the index of the neighbouring cell for each direction
+            _type, initial_resources, neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5 = [int(j) for j in input().split()]
+            neighs = [neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5]
+            self.cells.append(CellInfo(i, _type, initial_resources, neighs))
 
-cells = []
+    def update_input_data(self):
+        for i in range(self.number_of_cells):
+            # resources: the current amount of eggs/crystals on this cell
+            # my_ants: the amount of your ants on this cell
+            # opp_ants: the amount of opponent ants on this cell
+            resources, my_ants, opp_ants = [int(j) for j in input().split()]
+            self.cells[i].update(resources, my_ants, opp_ants)
 
-for i in range(number_of_cells):
-    # _type: 0 for empty, 1 for eggs, 2 for crystal
-    # initial_resources: the initial amount of eggs/crystals on this cell
-    # neigh_0: the index of the neighbouring cell for each direction
-    _type, initial_resources, neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5 = [int(j) for j in input().split()]
-    # debug(f"i[{i}] type[{_type}] initial_resources [{initial_resources}] neigh_0[{neigh_0}] neigh_1[{neigh_1}] neigh_2[{neigh_2}] neigh_3[{neigh_3}] neigh_4[{neigh_4}] neigh_5[{neigh_5}]")
-    cells.append(CellInfo(i, _type, initial_resources))
+    def get_best_cells(self, origin, turn):
+        output = ''
+        strength = 10
+        n_turns = 5
+        aux_cells = self.cells.copy()
+        aux_cells.sort(reverse=True)
 
-number_of_bases = int(input())
-for i in input().split():
-    my_base_index = int(i)
-    # debug(f"i[{i}] my_base_index[{my_base_index}]")
-for i in input().split():
-    opp_base_index = int(i)
-    # debug(f"i[{i}] opp_base_index[{opp_base_index}]")
-
-# game loop
-while True:
-    idx = -1
-    output = ''
-    res_cells = []
-    for i in range(number_of_cells):
-        # resources: the current amount of eggs/crystals on this cell
-        # my_ants: the amount of your ants on this cell
-        # opp_ants: the amount of opponent ants on this cell
-        resources, my_ants, opp_ants = [int(j) for j in input().split()]
-        cells[i].update(resources)
-        if cells[i].type == CellType.CRYSTAL:
-            res_cells.append((i, resources))
-        else:
-            res_cells.append((i, 0))
-
-    sorted(res_cells, key=lambda x: x[1])
-    strength = 10
-    for r in res_cells:
-        if r[1]:
-            output += f"LINE {my_base_index} {r[0]} {strength};"
-            strength -= 1
-    # print(f"LINE {my_base_index} {idx} 5")
-    print(f"{output[:len(output)-1]}")
-
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+        for c in aux_cells:
+            # debug(f"idx:{c.idx} type:{c.type.value} - {c.resources}")
+            if turn >= n_turns and c.type == CellType.CRYSTAL:
+                output += f"LINE {origin} {c.idx} {strength};"
+                strength -= 1
+            elif turn < n_turns and c.type == CellType.EGGS:
+                output += f"LINE {origin} {c.idx} {10};"
+                break
+        return output[:-1]
 
 
-    # WAIT | LINE <sourceIdx> <targetIdx> <strength> | BEACON <cellIdx> <strength> | MESSAGE <text>
-    # print("WAIT")
+class CodingameSpring():
+    def __init__(self):
+        self.turn = 0
+        self.map = Map()
+        self.number_of_bases = int(input())
+
+        for i in input().split():
+            self.my_base_index = int(i)
+        for i in input().split():
+            self.opp_base_index = int(i)
+
+        self.action = 'WAIT'
+
+    def loop(self):
+        while True:
+            self.get_new_input_data()
+            self.think()
+            self.output()
+            self.turn += 1
+
+    def get_new_input_data(self):
+        self.map.update_input_data()
+
+    def think(self):
+        self.action = self.map.get_best_cells(self.my_base_index, self.turn)
+
+    def output(self):
+        # Write an action using print
+        print(self.action)
+
+
+def main():
+    game = CodingameSpring()
+    game.loop()
+
+
+if __name__ == "__main__":
+    main()
